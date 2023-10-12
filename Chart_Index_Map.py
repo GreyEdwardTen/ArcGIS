052d92dc61987a281dd2b9dbfefe19cc96658ef9
GreyEdwardTen <62803345+GreyEdwardTen@users.noreply.github.com>
2023-10-12 17:06:58 +0100

    Update Chart_Index_Map.py
    Added code for DRAWING_NO and DRAWING_URL fields

--- Chart_Index_Map.py
+++ Chart_Index_Map.py
@@ -4,3 +4,19 @@ def ChartIndex(id):
     return "Sheet 005 of 005"
   else:
     return "Sheet 00" + str(id % 5) + " of 005"
+
+
+
+# DRAWING NUMBER
+# Select all the features of the same area and input their common names e.g., "GOSL-ZETR-KON-DWG-CL5"
+# Then we'll use it to recreate the full number by adding and the sheet number.
+# For example if we have already inputted DRAWING_NO = "GOSL-ZETR-KON-DWG-CL5" and for "Sheet 002 of 005":
+!DRAWING_NO! + "/" + !CHART_INDEX![-10:-7]
+#This should give you "GOSL-ZETR-KON-DWG-CL5/002"
+
+
+# DRAWING URL
+# First input your common hyperlink path to the folder in the Drawing URL field
+# e.g., "..\..\Document_Reports\Charts\CLUSTER 5\"
+# Then we'll use it to recreate the full URL by ading
+!DRAWING_URL! + "\\" + !DRAWING_NO!.replace("/", "-") + " - " + !DRAWING_NAME! + ".pdf"
